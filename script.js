4197b5a5efd8ad7b22ca9206323922f9c45eed40
ffemon <ffemon11boos@gmail.com>
2025-09-20 18:39:10 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Simple JavaScript interaction
+document.getElementById('alertButton').addEventListener('click', function() {
+    alert("Tips:\n- কখনো হ্যাক বা মড ব্যবহার করবেন না\n- আপনার অ্যাকাউন্ট নিরাপদ রাখুন\n- নিয়মিত গেম আপডেট চেক করুন");
+});
